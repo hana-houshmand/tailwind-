047e81bcf72ac6368ee8bc5202306b88e1181aa5
hana <hhoushmand76@gmail.com>
2024-05-15 17:14:41 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["vazir"],
+      },
+      fontSize: {
+        tiny: "8px",
+      },
+    },
+  },
+  plugins: [],
+};
